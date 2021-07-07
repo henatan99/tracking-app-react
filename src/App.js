8ce3f2b1795c13b276c2d73f397464d618914892
Henok <henatan99@gmail.com>
2021-07-08 01:40:02 +0300

    Fix eslint and styllelint errors

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
