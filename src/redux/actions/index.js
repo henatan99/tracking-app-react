ab1588a092ffd24651911599e7fb364c57a86c1c
Henok <henatan99@gmail.com>
2021-07-08 16:35:29 +0300

    Add CHANGE_USERNAME actionType in actionTypes.js and changeUsername action in index.js

---
+++ src/redux/actions/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import {
+    CHANGE_USERNAME,
+} from './actionTypes';
+
+export const changeUsername = (username) => ({
+    type: CHANGE_USERNAME,
+    payload: username,
+})
