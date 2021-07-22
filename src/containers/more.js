012396948c25caf6cda58e9b857029fc17259544
Henok <henatan99@gmail.com>
2021-07-22 19:54:23 +0300

    Add detail styling to more page

--- src/containers/more.js
+++ src/containers/more.js
@@ -12,15 +12,24 @@ const More = ({ user }) => {
   };
 
   return (
-    <div>
-      <h1>{user.username}</h1>
-      <ul>
-        <li>
-          <Link to={`/${user.id}/setgoal`}>Your Goal</Link>
-        </li>
-        <li>
-          <Link to="/" onClick={handleLogout}>Log Out</Link>
-        </li>
+    <div className="more">
+      <div className="user-profile">
+        <img className="user-img" src="" alt="" />
+        <h1 className="username">{user.username}</h1>
+      </div>
+      <ul className="more-ul">
+        <Link to={`/${user.id}/setgoal`} className="more-link">
+          <li className="more-li">
+            <span className="iconify" data-icon="flat-ui:goal" data-inline="false" />
+            <h3 className="more-li-title">Your Goal</h3>
+          </li>
+        </Link>
+        <Link to="/" onClick={handleLogout} className="more-link">
+          <li className="more-li">
+            <span className="iconify" data-icon="websymbol:logout" data-inline="false" />
+            <h3 className="more-li-title">Log Out</h3>
+          </li>
+        </Link>
       </ul>
     </div>
   );
