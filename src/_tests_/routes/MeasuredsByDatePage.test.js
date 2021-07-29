7295fcffe3987a945d07a93707c608d7eb3a73a8
Henok <henatan99@gmail.com>
2021-07-29 22:33:23 +0300

    Add test to MeasuredsByDatePage connected component

---
+++ src/_tests_/routes/MeasuredsByDatePage.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+import MeasuredsByDatePage from '../../containers/measureds';
+import createTestStore from '../factories/storeFactory';
+
+describe('Measureds', () => {
+    let store;
+    beforeEach(() => {
+      store = createTestStore();
+    });
+    test('should match with snapshot', () => {
+      const tree = renderer
+        .create(
+          <Provider store={store}>
+            <MemoryRouter>
+              <MeasuredsByDatePage />
+            </MemoryRouter>
+          </Provider>,
+        ).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+  
